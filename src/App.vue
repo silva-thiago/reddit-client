cf12e3ec4212c467dcf1390bbec24007e2f8a2be
thiagosilva <thiago.silva@imd.ufrn.br>
2020-10-16 16:38:29 -0300

    App default template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,93 @@
 <template>
-  <router-view/>
+  <nav class="nav-wrapper blue">
+    <div class="container">
+      <a href="#" class="brand-logo center">Reddit Client</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <!-- <li><a href="sass.html">Sass</a></li> -->
+      </ul>
+    </div>
+  </nav>
+  <router-view />
 </template>
 
 <style lang="scss">
+/** app styles **/
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+}
+
+html, body {
+  height: 100vh;
+}
+
+body {
+  background-color: rgb(247, 250, 252);
+  color: rgb(26, 32, 44);
+  font-family: 'Arial', serif;
+  font-feature-settings: 'liga'; // Support for IE
+  font-size: calc(1rem + 1vmin);
+  font-style: normal;
+  font-weight: 400;
+  letter-spacing: normal;
+  line-height: normal;
+  text-rendering: optimizeLegibility; // Support for Safari and Chrome
+  text-transform: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  /* browser scrollbar styles */
+  &::-webkit-scrollbar {
+    width: 0.75rem;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: rgb(247, 250, 252);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 0.5rem;
+    background-color: rgb(26, 32, 44);
+  }
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+li {
+  list-style: none;
+}
+
+main {
+  flex-shrink: 0;
+}
+
+.footer {
+  background: #edf2f7;
+  bottom: 0;
+  height: 3rem;
+  position: fixed;
+  width: 100%;
+
+  .container {
+    display: grid;
+    place-items: center;
+
+    .text-muted {
+      color: #718096;
+      font-size: 1rem;
+
+      a:hover {
+        color: #4a5568;
+      }
+    }
+  }
+}
 </style>
