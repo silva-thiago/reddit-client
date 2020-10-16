a227be1dd31bbba14e68966854848b85c8daf81e
thiagosilva <thiago.silva@imd.ufrn.br>
2020-10-16 16:38:43 -0300

    Index to Reddit Client

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Reddit Client"/>
+    <Reddit />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Reddit from '@/components/Reddit.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Reddit,
   },
 };
 </script>
